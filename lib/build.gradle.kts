7a1990316f167ee8a9ce20ee716b3bc37dd3727b
Sasa Blagojevic <sasa.blagojevic@mail.com>
2024-11-23 12:25:36 +0100

    bump version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,7 +10,7 @@ import org.jetbrains.kotlin.konan.properties.Properties
  * This project uses @Incubating APIs which are subject to change.
  */
 group = "tech.s-co"
-version = "0.1.4"
+version = "0.1.5"
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
