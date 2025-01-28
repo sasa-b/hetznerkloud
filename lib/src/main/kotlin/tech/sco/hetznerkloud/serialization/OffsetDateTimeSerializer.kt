75c16c0ffe6d25b3418a937b73afa5e8c5206664
Sasa Blagojevic <sasa.blagojevic@mail.com>
2025-01-28 11:06:46 +0100

    improve offset date time serializer

--- lib/src/main/kotlin/tech/sco/hetznerkloud/serialization/OffsetDateTimeSerializer.kt
+++ lib/src/main/kotlin/tech/sco/hetznerkloud/serialization/OffsetDateTimeSerializer.kt
@@ -1,18 +1,17 @@
 package tech.sco.hetznerkloud.serialization
 
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.OffsetDateTime
 
 object OffsetDateTimeSerializer : KSerializer<OffsetDateTime> {
-    override val descriptor: SerialDescriptor = String.serializer().descriptor
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(OffsetDateTime::class.qualifiedName!!, PrimitiveKind.LONG)
 
-    override fun serialize(encoder: Encoder, value: OffsetDateTime) =
-        encoder.encodeInline(descriptor).encodeString(value.toString())
+    override fun serialize(encoder: Encoder, value: OffsetDateTime) = encoder.encodeInline(descriptor).encodeString(value.toString())
 
-    override fun deserialize(decoder: Decoder): OffsetDateTime =
-        OffsetDateTime.parse(decoder.decodeInline(descriptor).decodeString())
+    override fun deserialize(decoder: Decoder): OffsetDateTime = OffsetDateTime.parse(decoder.decodeInline(descriptor).decodeString())
 }
